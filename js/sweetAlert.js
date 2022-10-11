5086de54d06682e4b070236cb3c348fc637075de
saaw <sergioaast@gmail.com>
2022-10-11 12:43:16 -0300

    update categories img and add sweetAlert

---
+++ js/sweetAlert.js
@@ -0,0 +1,16 @@
+ 
+
+  Swal.fire({
+    title: 'Bienvenidos a NO-PET SHOP',
+    imageUrl: 'https://img.freepik.com/free-photo/chubby-domestic-cat-leaning-brown-puppy-lying-white-surface_181624-45927.jpg?w=1380&t=st=1665502146~exp=1665502746~hmac=7511d35e08dca49fe701ee9409e0a879a4fdff5815407765567b7dffad84e087',
+    imageWidth: 900,
+    imageHeight: 220,
+    imageAlt: 'Custom image',
+    width: 600,
+    padding: '3em',
+    backdrop: `
+      rgba(0,0,123,0.4)
+      left top
+      no-repeat
+    `
+  })
